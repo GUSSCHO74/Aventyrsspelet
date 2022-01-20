b4275cdce66c696b3c669ba558f9b979ae90153d
GUSSCH <steamjg04@gmail.com>
2022-01-20 21:36:09 +0100

    fixed if not Y/N + playagain + small text details

--- projekt.py
+++ projekt.py
@@ -16,6 +16,21 @@ class Item():
         self.name = name
         self.strength = strength
 
+diamond_sword = Item("Diamond sword", 25)
+iron_sword = Item("Iron sword", 20)
+gold_sword = Item("Gold sword", 15)
+stone_sword = Item("Stone sword", 10)
+wooden_sword = Item("Wooden sword", 5)
+
+list_of_items = [diamond_sword, iron_sword, gold_sword, stone_sword, wooden_sword]
+
+def item_exchange(player):
+    worst = Item("", 100)
+    for item in player.inv:
+        if worst.strength > item.strength:
+            worst = item
+    player.inv.remove(worst)
+
 def door(player):
     door_type = random.choice(["Treasure", "Trap", "Monster"]) 
     if door_type == "Treasure":
@@ -31,57 +46,62 @@ def door(player):
             print("Your inventory right now:")
             player.print_inventory()
             print()
-            exchange = input("Type: Y/N (Y for Yes, N for No) ")
-            if exchange == "Y": 
-                item_exchange(player)
-                player.inv.append(random_treasure)
-                player.strength = player.strength + random_treasure.strength
-            elif exchange == "N":  
-                print("You left the treasure and countinued your journey")
-            else:
-                print("You need to type Y or N (Y for Yes, N for No):\n")
+            while True:
+                exchange = input("Type: Y/N (Y for Yes, N for No) ")
+                print()
+                if exchange == "Y": 
+                    item_exchange(player)
+                    player.inv.append(random_treasure)
+                    player.strength = player.strength + random_treasure.strength
+                    print("The sword has been added to your inventory.\n")
+                    break
+                elif exchange == "N":  
+                    print("You've left the treasure and countinued your journey.\n")
+                    break
+                else:
+                    print("You need to type Y or N (Y for Yes, N for No): ")
+                    print()
     elif door_type == "Trap":
         trap_damage = random.randint(1,3)
         player.hp = player.hp - trap_damage
-        print("You encountered a trap")
-        print(f"The trap dealt {trap_damage} damage\n")
+        print("You encountered a trap!")
+        print(f"The trap dealt {trap_damage} damage.\n")
     elif door_type == "Monster":
-        monster_strength = random.randint(1, 90)
-        print(f"The monster has a strength of {monster_strength}")
+        monster_strength = random.randint(1, 80)
+        print(f"The monster has a strength of {monster_strength}!")
         if monster_strength > player.strength:
             player.hp = player.hp - 1
-            print("The monster won the battle and you lost 1 hp\n")
+            print("The monster won the battle and you lost 1 hp.\n")
         elif monster_strength < player.strength:
             player.lvl = player.lvl + 1
-            print("You won the battle against the monster")
-            print("Your level went up by one point")
+            print("You've won the battle against the monster.")
+            print("Your level went up by one point!")
             print()
-
-diamond_sword = Item("Diamond sword", 25)
-iron_sword = Item("Iron sword", 20)
-gold_sword = Item("Gold sword", 15)
-stone_sword = Item("Stone sword", 10)
-wooden_sword = Item("Wooden sword", 5)
-
-list_of_items = [diamond_sword, iron_sword, gold_sword, stone_sword, wooden_sword]
-
-def item_exchange(player):
-    worst = Item("", 100)
-    for item in player.inv:
-        if worst.strength > item.strength:
-            worst = item
-    player.inv.remove(worst)
               
 def start_game():
-    player = Player(1,20,0,[])
-    if player.lvl == 10:
-        print("Congratulations, you won")
-    while player.hp > 0: 
+    player = Player(1,25,0,[])
+    while player.lvl < 10: 
+        if player.hp <= 0: 
+            print("YOU DIED!")
+            while True:
+                play_again = input("DO YOU WANT TO PLAY AGAIN? Y/N: ")
+                print()
+                if play_again == "Y":
+                    start_game()
+                elif play_again == "N":
+                    print ("We've had fun with you, see you next time!")
+                    print()
+                    exit()
+                else:
+                    print("You need to type Y or N (Y for Yes, N for No): ")  
+                    print()
         choice = input("What would you like to do?\nA: Check your stats\nB: Pick your door\nC: Check your inventory\n\nYOUR CHOICE: ")
         if choice == "A":
             print()
-            print("YOUR STATS:")  
-            print(f"\nCurrent Level: {player.lvl}\nYour HP: {player.hp}\nStrength: {player.strength}\n")
+            print("YOUR STATS:")
+            print("-----------------")  
+            print(f"Current Level: {player.lvl}\nYour HP: {player.hp}\nStrength: {player.strength}")
+            print("-----------------\n")  
         elif choice == "B":
             print()
             door(player)
@@ -92,13 +112,20 @@ def start_game():
             print()
         else:
             print()
-            print("Please type either A, B or C\n")
+            print("Please type either A, B or C.\n")
     else:
-        print("You died")
-        play_again = input("DO YOU WANT TO PLAY AGAIN? Y/N:\n ")
-        if play_again == "Y":
-            start_game()
-        else:
-            print("Fuck you!")  
+        print("Congratulations, you won!")
+        while True:
+            play_again = input("DO YOU WANT TO PLAY AGAIN? Y/N: ")
+            print()
+            if play_again == "Y":
+                start_game()
+            elif play_again == "N":
+                print ("We've had fun with you, see you next time!")
+                print()
+                exit()
+            else:
+                print("You need to type Y or N (Y for Yes, N for No): ")
+                print()
 
-start_game()
+start_game() 
